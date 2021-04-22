14c7e57b38a44da2171fb32b42e1d4d1e4d1568a
mendesflavia <mendesflavia@hotmail.com>
2021-04-22 18:43:25 -0300

    2 aula

--- src/componentes/Header/index.tsx
+++ src/componentes/Header/index.tsx
@@ -1,13 +1,13 @@
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
 
-import styles from'./styler.module.scss';
+import styles from'./styles.module.scss';
 export function Header(){   
-     const currentDate = format(new Date(),'EEEEEE,d MMMM' {
-      locale: ptBR   
+     const currentDate = format(new Date(),'EEEEEE,d MMMM'), {
+     locale: ptBR
      }
     return(
-        <header className={styler.headerContainer} >
+        <header className={styles.headerContainer} >
          
             <img src="/logo.svg" alt="Podcastr"/>
 
