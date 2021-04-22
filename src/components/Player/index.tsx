88c69038611d3c54e6de899b932ec56987e8ad42
mendesflavia <mendesflavia@hotmail.com>
2021-04-22 20:47:54 -0300

    2 aula

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,7 +1,7 @@
 import styles from './styles.module.scss';
 export function Player(){   
     return(
-        <div>
+        <div className={styles.playerContainer}>
             
 
         </div>
